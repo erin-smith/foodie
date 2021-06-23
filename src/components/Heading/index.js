5c936e38e2fad62b48dc2bf1d3f0c21c9fa17cbb
Erin Smith <erin.acumen@gmail.com>
2021-06-22 17:17:18 -0700

    use own images, begin to size

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 
 function Heading(){
 return(
-
-<div className="container">
         <div className="row">
             <div className="col-xs-12">
                 <h1 className="text-center">Chef Wisdom</h1>
                 <p className= "lead text-center">"It's around the table and in the preparation of food that we learn about ourselves and about the world. --Alice Waters"</p>
-                <p className= "lead text-center">“But once in a while you might see me at In and Out Burger; they make the best fast food hamburgers around. --Thomas Keller”</p>
-                <p className="lead text-center">"I don't think I'm a chef.  I'm just someone that...found a way of speaking.  It was not a pen.  It was not a brush.  It was food. -- Dominique Crenn"</p>
+                <p className="lead text-center">"I don't think I'm a chef.  I...found a way of speaking.  It was not a pen.  It was not a brush.  It was food. -- Dominique Crenn"</p>
                 <p className="lead text-center"> "People who love to eat are always the best people. -- Julia Child" </p>
             </div>
         </div>
-    </div>
 )
 };
 
--- src/pages/Local/index.js
+++ src/pages/Local/index.js
@@ -5,6 +5,7 @@ function Local (){
 <div>
     <h1>San Diego Local Favorites</h1>
     <h3>"You don't need a silver fork to eat good food. -Paul Prudhomme"</h3>
+    <h3>“But once in a while you might see me at In and Out Burger; they make the best fast food hamburgers around. --Thomas Keller”</h3>
 <Card/>
 </div>
 
