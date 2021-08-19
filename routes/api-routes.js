f62a96123cfa23c073de1b63f91d2eb2bd5556a8
Erin Smith <erin.acumen@gmail.com>
2021-08-19 05:29:28 -0700

    config env variables

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,50 +22,50 @@ module.exports = function (app) {
   });
 
 });
-app.get("/api/chefs", (req, res) => {
-      chefs.findAll({})
-      .then(data => {
-        // res.send(data);
-        let chefs = data.map(({name, awards, restaurants, city}) => ({
-          name: name,
-          awards: awards,
-          restaurants: restaurants,
-          city: city
-        }));
+// app.get("/api/chefs", (req, res) => {
+//       chefs.findAll({})
+//       .then(data => {
+//         // res.send(data);
+//         let chefs = data.map(({name, awards, restaurants, city}) => ({
+//           name: name,
+//           awards: awards,
+//           restaurants: restaurants,
+//           city: city
+//         }));
 
-        let chefObject = {chefs : chefs };
-        res.render("/transformative", chefObject)
-      })
-      .catch(err => {
-        res.status(500).send({
-        message:
-        err.message || "Error occured while creating chefs."
-      });
-    });
-        // results are available to us inside the .then
-      }),
+//         let chefObject = {chefs : chefs };
+//         res.render("/transformative", chefObject)
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//         message:
+//         err.message || "Error occured while creating chefs."
+//       });
+//     });
+//         // results are available to us inside the .then
+//       }),
 
-      app.get("/api/locals", (req, res) => {
-      locals.findAll({})
-      .then(data => {
-        // res.send(data);
-        let locals = data.map(({name, awards, restaurants, city}) => ({
-          name: name,
-          awards: awards,
-          restaurants: restaurants,
-          city: city
-        }));
+//       app.get("/api/locals", (req, res) => {
+//       locals.findAll({})
+//       .then(data => {
+//         // res.send(data);
+//         let locals = data.map(({name, awards, restaurants, city}) => ({
+//           name: name,
+//           awards: awards,
+//           restaurants: restaurants,
+//           city: city
+//         }));
 
-        let localObject = {locals: locals};
-        res.render("/local", localObject)
-      })
-      .catch(err => {
-        res.status(500).send({
-        message:
-        err.message || "Error occured while creating chefs."
-      });
-    });
-  });
+//         let localObject = {locals: locals};
+//         res.render("/local", localObject)
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//         message:
+//         err.message || "Error occured while creating chefs."
+//       });
+//     });
+//   });
 
 
 };
