fa87df40dc5558301ecd35b51477ef86530566f3
Erin Smith <erin.acumen@gmail.com>
2021-08-26 12:35:20 -0700

    configure vars

--- db/models/index.js
+++ db/models/index.js
@@ -23,7 +23,11 @@ const config = {
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'JAWSDB_URL',
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
     dialect: 'mysql',
   },
 }[env];
